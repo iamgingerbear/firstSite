c6007249884643b415d1c6b362ac4d1fd2c13f81
Jack Davies <gingerbeargames@gmail.com>
2019-01-29 18:05:46 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// the component template, insert what you want to be rendered
+// between the div tags
+// render the component in the main app using:
+// <myFirstComponent></<myFirstComponent>
+
+Vue.component('myFirstComponent', {
+    template: `
+    <div>
+        <h1>Welcome To My First Site</h1>
+    </div>
+    `,
+    data: function(){
+        return {
+
+        }
+    }
+})
+
+
+// the main vue component that will order the other components
+var app = new Vue({
+    template: `
+    <div class="container">
+        <myFirstComponent></<myFirstComponent>
+    </div>    
+    `,
+    el: '#app',
+    data: {
+        
+    },
+    methods: {
+
+    }
+})
