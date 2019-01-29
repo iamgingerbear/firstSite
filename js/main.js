6fa3d806693e9cd43aad4d0002aee3005d4fffe6
Jack Davies <gingerbeargames@gmail.com>
2019-01-29 22:51:23 +0000

    day1 done

--- js/main.js
+++ js/main.js
@@ -3,27 +3,50 @@
 // render the component in the main app using:
 // <myFirstComponent></<myFirstComponent>
 
-Vue.component('myFirstComponent', {
+Vue.component('banner', {
     template: `
-    <div>
-        <h1>Welcome To My First Site</h1>
-        <img src="../img/hi.jpg">
-    </div>
+    <header>
+        <h1>This is my Title</h1>
+        <img src="https://data.whicdn.com/images/252588371/original.gif">
+    </header>        
     `,
     data: function(){
         return {
-
+        
         }
     }
 })
 
+Vue.component('boxes', {
+    template: `
+    <div>
+        <h2>Portfolio</h2>
+        <div v-for="item in items">
+            <h3>{{item.name}}</h3>
+            <img :src="item.img">
+            <p>{{item.text}}</p>
+        </div>
+    </div>
+    `,
+    data: function(){
+        return {
+            items: {
+                'first item':{
+                    img: "https://media3.giphy.com/media/VS2BAGAeti2qI/source.gif",
+                    name: "first item",
+                    text: "This is some stuff about this image, isn't it",
+                },
+            }
+    }
+}})
 
 // the main vue component that will order the other components
 var app = new Vue({
     template: `
     <div class="container">
-        <myFirstComponent></<myFirstComponent>
-    </div>    
+        <banner></banner>
+        <boxes></boxes>
+    </div>
     `,
     el: '#app',
     data: {
@@ -33,3 +56,43 @@ var app = new Vue({
 
     }
 })
+
+// sams mess
+// Vue.component('MowMow', {
+//     template: `
+//     <Header>
+//     <h1>Satan did it</h1>
+//     <img src="https://www.catster.com/wp-content/uploads/2018/05/A-gray-cat-crying-looking-upset.jpg">
+
+// </Header>
+//     `,
+//     data: function(){
+//         return {
+
+//         }
+//     }
+// })
+
+// Vue.component('Bumpkin', {
+//     template: `
+//     <div>
+//     <h2>Portfolio</h2>
+//     <div v-for="item in items">
+//         <h3>{{item.name}}</h3>
+//         <img :src="item.img">
+//         <p>{{item.text}}</p>
+//     </div>
+// </div>
+//     `,
+//     data: function(){
+//         return {
+//             items: {
+//                 'Death':{
+//                     img: "https://pics.me.me/smirnof-its-an-intellectual-mana-potion-for-the-chosen-ones-37617286.png",
+//                     name: "Death",
+//                     text: "Fuck off",
+//                 },
+//             }
+//         }
+//     }
+// })
